7d86cf3f2bfd5e66631e0b185ffc3f7f0efa506b
Eric Vincent Fomo <evfomo@gmail.com>
2024-01-26 10:55:59 -0600

    jenkins

---
+++ scripts/git.sh
@@ -0,0 +1,6 @@
+# How to use a shell script to push code to Github?
+
+read -r -p 'Commit message: ' desc  # prompt user for commit message
+git add .                           # track all files
+git commit -m "$desc"               # commit with message
+git push -u origin master           # push to origin
